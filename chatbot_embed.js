34a6ff750c16dbedeaf1d1a2071fe237cde1f43b
Anmol Raj Soni <anmolrajsoni15@gmail.com>
2023-07-20 12:08:03 +0530

    modification

--- chatbot_embed.js
+++ chatbot_embed.js
@@ -1,13 +1,32 @@
+// Create the iframe element
+var iframe = document.createElement("iframe");
+iframe.id = "myIframe";
+iframe.style.position = "fixed";
+iframe.style.width = "100px";
+iframe.style.height = "100px";
+iframe.style.border = "none";
+iframe.style.bottom = 0;
+iframe.style.right = 0;
+iframe.style.zIndex = 1050;
+iframe.style.background = "transparent";
+document.body.appendChild(iframe);
+
+// Add event listener to receive messages from the parent source
 window.addEventListener("message", receiveMessage, false);
 function receiveMessage(event) {
   var parameter = event.data;
   if (typeof parameter === "object" && parameter !== null) {
     parameter = JSON.stringify(parameter);
   }
-  if (parameter === "open") {
+
+  // Set the src attribute of the iframe
+  var iframeSrc = parameter.srcUrl;
+  document.getElementById("myIframe").src = iframeSrc;
+
+  if (parameter.status === "open") {
     document.getElementById("myIframe").style.width = "350px";
     document.getElementById("myIframe").style.height = "600px";
-  } else if (parameter === "close") {
+  } else if (parameter.status === "close") {
     setTimeout(() => {
       document.getElementById("myIframe").style.width = "100px";
       document.getElementById("myIframe").style.height = "100px";
